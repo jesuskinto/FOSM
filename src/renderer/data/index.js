f0e59277fd3ee011ed41817ae36b3c6cfd8c6b4a
migherize <36181233+migherize@users.noreply.github.com>
2022-01-25 18:48:40 -0400

    add all level but under information is kinder.

--- src/renderer/data/index.js
+++ src/renderer/data/index.js
@@ -1,5 +1,10 @@
 import level0 from './level0.json'
 import level1 from './level1.json'
+import level2 from './level2.json'
+import level3 from './level3.json'
+import level4 from './level4.json'
+import level5 from './level5.json'
+import level6 from './level6.json'
 
 const initialData = {
   user: {
@@ -8,7 +13,12 @@ const initialData = {
   },
   levels: [
     level1,
-    level0
+    level0,
+    level2,
+    level3,
+    level4,
+    level5,
+    level6
   ]
 }
 
